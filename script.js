c3c36d53ec39335313dc245c560dee90b19b49c4
Pedrinhzx <joaopedroferreirabritoferreira@gmail.com>
2024-07-22 16:58:54 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let firstName = "João";
+let lastName = "Silva";
+let maritalStatus = "solteiro";
+let country = "Brasil";
+let age = 30;
+ 
+console.log(`
+Meu nome é ${firstName} ${lastName}. Minha idade é ${age}. Meu estado civil é ${maritalStatus}. Eu moro no ${country}
+`);
